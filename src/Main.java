c3d0b04c664a02139df2cc94502c627698827a74
Jan Giercke <jan@giercke.de>
2022-11-14 12:21:38 +0100

    code cleanup and comments

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,7 @@ public class Main {
     public static void main(String[] args) {
 
         try {
-            TestSettings testSettings = TestSettings.loadSettings("2022-isys2-data/test1.txt");
+            TestSettings testSettings = TestSettings.loadSettings("2022-isys2-data/test2.txt");
             System.out.println(testSettings);
         } catch (IOException e) {
             throw new RuntimeException(e);
--- src/Position.java
+++ src/Position.java
@@ -1,25 +1,24 @@
+/**
+ * Stores a 2D-position
+ */
 public final class Position {
     Double x;
     Double y;
 
+    /**
+     * Constructor for Position
+     * @param x int: x-coordinate
+     * @param y int: y-coordinate
+     */
     public Position(Double x, Double y) {
         this.x = x;
         this.y = y;
     }
 
-    public Double x() {
-        return x;
-    }
+    public Double x() { return x; }
 
-    public Double y() {
-        return y;
-    }
+    public Double y() { return y; }
 
     @Override
-    public String toString() {
-        return "{" +
-                x +
-                ", " + y +
-                '}';
-    }
+    public String toString() { return "{" + x + ", " + y + '}'; }
 }
